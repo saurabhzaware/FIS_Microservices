c7d40c996c172137e2fd7badac98d04dc940b127
saurabhzaware <saurabhzaware1199@gmail.com>
2021-08-16 12:57:20 +0530

    Implementing Loan Microservice

---
+++ loan/src/main/java/com/fis/loan/controller/LoanController.java
@@ -0,0 +1,20 @@
+package com.fis.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fis.loan.bean.Loan;
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+	
+
+		@GetMapping("/{number}")
+		public Loan getLoan(@PathVariable long number) {
+			return new Loan(987987972342l, "car", 400000, 3258, 18);
+		}
+
+	}
+
